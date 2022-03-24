2db574df00aa5c3dfae4f7d49a4e238410b6d008
Caíque Fernandes <caiiqe.fernandes@hotmail.com>
2022-03-23 21:25:30 -0300

    update - project is working

--- src/pages/details/index.js
+++ src/pages/details/index.js
@@ -1,12 +1,48 @@
-import { useParams } from "react-router-dom"
-
-function Details(){
-
-    const {id} = useParams()
-    console.log(id)
+import { useState, useEffect } from 'react';
+import { Link, useParams } from "react-router-dom"
+import { APIKey } from "../../config/key"
+import { Container } from './styles';
 
 
-    return <h1>Página de Detalhes!</h1>
+function Details() {
+
+    const { id } = useParams()
+
+    const [movie, setMovie] = useState({})
+    const image_path = 'https://image.tmdb.org/t/p/w500/'
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${APIKey}&language=en-US&page=1`)
+            .then(response => response.json())
+            .then(data => {
+
+                const { title, poster_path, overview, release_date, runtime } = data
+                const movie = {
+                    id,
+                    title,
+                    sinopse: overview,
+                    image: `${image_path}${poster_path}`,
+                    releaseDate: release_date,
+                    runtime: runtime
+                }
+                setMovie(movie)
+            })
+    }, [id])
+
+    return (
+        <Container>
+            <div className="movie">
+                <img src={movie.image} alt={movie.sinopse} />
+                <div className="details">
+                    <h1>{movie.title}</h1>
+                    <span>Sinopse: {movie.sinopse}</span>
+                    <span className='release-date'>Data de Lançamento: {movie.releaseDate}</span>
+                    <span>Duração: {movie.runtime} minutos</span>
+                    <Link to="/"><button>Voltar</button></Link>                    
+                </div>
+            </div>
+        </Container>
+    )
 }
 
 export default Details
---
+++ src/pages/details/styles.js
@@ -0,0 +1,62 @@
+import  styled  from 'styled-components'
+
+export const Container = styled.div`
+
+    padding: 4rem 0;
+
+
+    h1 {
+        margin: 3rem 0;
+    }
+
+    .movie {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    img {
+        width: 300px;
+        border-radius: 1rem;
+        transition: all 2s;
+        box-shadow: 0px 0px 15px #ffffff; 
+    }
+
+    img:hover{
+        transform: scale(1.150);
+    }
+
+    .details {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        margin-left: 3rem;
+        max-width: 50%;
+    }
+
+    button{
+        background: #6654da;
+        border: none;
+        cursor: pointer;
+        border-radius: 1rem;
+        color: white;
+        padding: 0.8rem 2rem;
+        margin-top: 1rem;
+        font-size: 100%;
+        transition: all 0.3s;
+    }
+
+    button:hover{
+        background: #5848c2;
+    }
+
+    span{
+        line-height: 130%;
+        margin-bottom: 1rem;
+        font-size: 110%;
+    }
+
+    .release-date{
+        opacity: 0.5;
+    }
+`
